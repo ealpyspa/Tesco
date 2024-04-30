eeacdb7da4b4363edcb28f66bdfa832af1adb16c
ealpyspa <alpyspaeva.en@gmail.com>
2024-04-30 13:39:27 +0200

    day3 - TotalPriceTest

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -96,5 +96,4 @@ public class HomePage extends BasePage {
         removeBananaBtn.click();
     }
 
-
 }
---
+++ src/test/java/tests/TotalPriceTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import basetestitems.BaseTest;
+import driver.Settings;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class TotalPriceTest extends BaseTest {
+    @Test
+    public void testTotalPriceCalculation() {
+        try {
+            HomePage homePage = new HomePage(driver);
+            LoginPage loginPage = new LoginPage(driver);
+            homePage.openWebsite();
+            homePage.acceptCookies();
+            homePage.clickOnSignInButton();
+            loginPage.login(Settings.EMAIL, Settings.PASSWORD);
+            homePage.clickOnFruitAndVegetablesTab();
+            homePage.clickOnAllFruitAndVegetablesTab();
+            homePage.addBananaToBasket();
+            homePage.addCucumberToBasket();
+            homePage.clickOnBasketButton();
+            Thread.sleep(2000);
+
+            String expectedTotalPrice = "525";
+            String actualTotalPrice = homePage.getTotalPrice();
+            Thread.sleep(2000);
+
+            Assertions.assertEquals(expectedTotalPrice, actualTotalPrice);
+
+            homePage.removeCucumber();
+            Thread.sleep(3000);
+            homePage.removeBanana();
+            Thread.sleep(3000);
+
+        } catch (Exception e) {
+            Assertions.fail(e.getMessage());
+        }
+    }
+}
